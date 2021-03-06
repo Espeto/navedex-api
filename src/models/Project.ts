ea08c8763f3a29bc57af356ffcac32178eb61006
Espeto <gkreisler5@hotmail.com>
2021-03-06 17:14:23 -0300

    fix: duplicated created_at

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import User from './User';
 
 @Entity('projects')
-class Project {}
+class Project {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('int')
+  owner_id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'owner_id' })
+  owner: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
 
 export default Project;
