4a89deb6cc1ae9193d279aaafc18b8e22a1cc2e9
Espeto <gkreisler5@hotmail.com>
2021-03-05 23:28:03 -0300

    fix: project require to use int as id

--- src/database/migrations/1614959205796-CreateProjects.ts
+++ src/database/migrations/1614959205796-CreateProjects.ts
@@ -8,10 +8,10 @@ export default class CreateProjects1614959205796 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'uuid',
+            type: 'int',
             isPrimary: true,
-            generationStrategy: 'uuid',
-            default: 'uuid_generate_v4()',
+            isGenerated: true,
+            generationStrategy: 'increment',
           },
           {
             name: 'name',
