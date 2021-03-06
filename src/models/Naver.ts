f08d4070bc93b02e8f1197cf5b583443a6f8a810
Espeto <gkreisler5@hotmail.com>
2021-03-06 17:15:57 -0300

    feat: added relation fields

--- src/models/Naver.ts
+++ src/models/Naver.ts
@@ -1,6 +1,51 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  ManyToMany,
+  JoinTable,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import User from './User';
+import Project from './Project';
 
 @Entity('navers')
-class Naver {}
+class Naver {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('date')
+  birthdate: Date;
+
+  @Column('date')
+  admission__date: Date;
+
+  @Column()
+  job_role: string;
+
+  @Column('int')
+  owner_id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'owner_id' })
+  owner: User;
+
+  @ManyToMany(() => Project)
+  @JoinTable()
+  projects: Project[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
 
 export default Naver;
