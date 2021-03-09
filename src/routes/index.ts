e44ca787b6db2747efb8a47271c83b2fbd8d9969
Espeto <gkreisler5@hotmail.com>
2021-03-09 12:32:14 -0300

    feat: added navers route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
+import naversRouter from './navers.routes';
 
 const routes = Router();
 
 routes.use('/signup', usersRouter);
 routes.use('/login', sessionsRouter);
+routes.use('/navers', naversRouter);
 
 export default routes;
