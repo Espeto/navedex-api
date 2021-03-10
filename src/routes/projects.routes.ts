4b1ce8e3931a60ee5db0d2b3ef1941610f360c62
Espeto <gkreisler5@hotmail.com>
2021-03-10 13:20:34 -0300

    feat: filter and detail projects route

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,11 +1,38 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateProjectService from '../services/CreateProjectService';
 
+import ProjectsRepository from '../repositories/ProjectsRepository';
+
 const projectsRouter = Router();
 projectsRouter.use(ensureAuthenticated);
+
+projectsRouter.get('/index', async (request, response) => {
+  const { name } = request.body;
+  const owner_id = request.user.id;
+
+  const projectsRepository = getCustomRepository(ProjectsRepository);
+
+  const projects = projectsRepository.filterProjects({
+    name,
+    owner_id,
+  });
+
+  return response.json(projects);
+});
+
+projectsRouter.get('/show/:id', async (request, response) => {
+  const { id } = request.params;
+  const owner_id = request.user.id;
+
+  const projectsRepository = getCustomRepository(ProjectsRepository);
+
+  const project = projectsRepository.detailProjectById({ owner_id, id });
+
+  return response.json(project);
+});
 
 projectsRouter.post('/store', async (request, response) => {
   const { name, navers } = request.body;
